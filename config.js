8dd34cae6992d357e5efb146e6387f15a3c26635
durga598 <durga598@gmail.com>
2016-12-12 17:52:56 +0530

    files added

---
+++ config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC5Rh4Sz7LoXk06aG8r4dRnqFJqD6kdz_s",
+    authDomain: "single-app-bd2af.firebaseapp.com",
+    databaseURL: "https://single-app-bd2af.firebaseio.com",
+    storageBucket: "single-app-bd2af.appspot.com",
+    messagingSenderId: "1013183149533"
+  };
+  firebase.initializeApp(config);
+//   console.log(firebase)
